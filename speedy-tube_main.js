7251f734bf5b94e8d735d1b392d8926469dfcf5e
groomyjohny <groomyjohny@gmail.com>
2021-12-19 04:31:42 +0500

    Added a bit of logging

--- speedy-tube_main.js
+++ speedy-tube_main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Speedy Tube
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.3.2
 // @description  A script to make YouTube videos play faster than 2x 
 // @author       groomyjohny
 // @include      https://*youtube.com/*
@@ -9,11 +9,15 @@
 // @downloadURL  https://github.com/groomyjohny/Speedy-Tube/blob/main/speedy-tube_main.js
 // ==/UserScript==
 
-let configuredSpeed = 3;
-window.addEventListener("load", function(){
-    let videos = document.getElementsByTagName("video");
-    for (v in videos)
-    {
-        v.playbackSpeed = configuredSpeed;
-    }
+let configuredRate = 3;
+window.addEventListener("load", () => {
+    console.log("Waking up changing...")
+    //this.setTimeout(() => {
+        let videos = document.getElementsByTagName("video");
+        for (let v in videos)
+        {
+            console.log("Chaning speed...",v.playbackRate = configuredRate);
+        }
+   // })
+   
 })
