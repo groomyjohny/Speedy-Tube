40de19d4df96e712d80a024d96319383f3fed26a
groomyjohny <groomyjohny@gmail.com>
2021-12-19 04:06:05 +0500

    v0.2 added download URL

--- speedy-tube_main.js
+++ speedy-tube_main.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Speedy Tube
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  A script to make YouTube videos play faster than 2x 
 // @author       groomyjohny
 // @include      https://*youtube.com/*
 // @grant        none
+// @downloadURL  https://github.com/groomyjohny/Speedy-Tube/blob/main/speedy-tube_main.js
 // ==/UserScript==
 
 let configuredSpeed = 3;
